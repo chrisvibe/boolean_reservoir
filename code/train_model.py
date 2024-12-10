c68d3ea3965247a8a31a296a4880b2dadcd111ad
chrisvibe <vibechris@gmail.com>
2024-12-10 21:28:56 +0100

    forgot to save file...

--- code/train_model.py
+++ code/train_model.py
@@ -41,10 +41,10 @@ if __name__ == '__main__':
     # encoding = 'base2'
     encoding = 'binary_embedding'
     n_inputs = 1
-    bits_per_feature = 20  # Number of bits per dimension
+    bits_per_feature = 16  # Number of bits per dimension
 
     # Parameters: Reservoir Layer
-    n_nodes = 1000
+    n_nodes = 100
     max_connectivity = 5
     avg_k = 2
 
